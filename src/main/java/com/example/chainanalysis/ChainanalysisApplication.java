bf05766fd301d12e93f2825ed441b8e67d2bcdad
Kavya9294 <kavya.ub9294@gmail.com>
2020-08-06 19:43:27 -0400

    Deleted database configuration as it was not required and cleaned up code

--- src/main/java/com/example/chainanalysis/ChainanalysisApplication.java
+++ src/main/java/com/example/chainanalysis/ChainanalysisApplication.java
@@ -1,12 +1,10 @@
 package com.example.chainanalysis;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,15 +12,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 
-@Configuration
-@ComponentScan
-@ComponentScan
-@EnableAutoConfiguration
 public class ChainanalysisApplication {
-	private static final Logger log = LoggerFactory.getLogger(ChainanalysisApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(ChainanalysisApplication.class, args);
+		SpringApplication.run(ChainanalysisApplication.class);
 	}
 
 	@Bean
@@ -30,14 +28,6 @@ public class ChainanalysisApplication {
 		return builder.build();
 	}
 
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
-		return args -> {
-			Coinbase cb=restTemplate.getForObject("https://api.coinbase.com/v2/prices/spot?currency=USD", Coinbase.class);
-			log.info(cb.toString());
-
-		};
-	}
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -49,4 +39,5 @@ public class ChainanalysisApplication {
 			}
 		};
 	}
+
 }
